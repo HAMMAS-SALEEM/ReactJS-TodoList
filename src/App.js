671fb365c21a4eed7610d4a4aff50ef6b594d634
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-16 17:51:45 +0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import logo from './logo.svg';
+import * as React from 'react';
+// import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import AboutApp from './components/js/AboutApp';
+import AboutAuthor from './components/js/AboutAuthor';
+import Home from './components/js/Homepage';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+      <AboutApp />
+      <AboutAuthor />
+    </>
   );
 }
-
-export default App;
---
+++ src/components/js/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <h1 className="homepage-title">Welcome to React TodoList!</h1>
+    </div>
+  );
+}
