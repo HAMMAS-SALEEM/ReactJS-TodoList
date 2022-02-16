235f74e2ae6f96fdb4affb5f6da02b75064d5402
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-17 04:00:48 +0500

    Routing is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import * as React from 'react';
-// import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
-import AboutApp from './components/js/AboutApp';
-import AboutAuthor from './components/js/AboutAuthor';
-import Home from './components/js/Homepage';
+import NavBar from './components/js/NavBar';
 
 export default function App() {
   return (
     <>
-      <Home />
-      <AboutApp />
-      <AboutAuthor />
+      <NavBar />
     </>
   );
 }
--- src/components/js/Homepage.js
+++ src/components/js/Homepage.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Form from './Form';
 import '../css/Homepage.css';
 import Todos from './Todos';
+import NavLinks from './NavLinks';
 
 export default function Home() {
   return (
-    <section className="App">
-      <h1 className="homepage-title">Welcome to React TodoList!</h1>
-      <Form />
-      <ul className="todos-container">
-        <Todos />
-      </ul>
-    </section>
+    <>
+      <NavLinks />
+      <section className="App">
+        <h1 className="homepage-title">Welcome to React TodoList!</h1>
+        <Form />
+        <ul className="todos-container">
+          <Todos />
+        </ul>
+      </section>
+    </>
   );
 }
